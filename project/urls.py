024ac9b0e27bbf0aed6f75f6a3f7aad650d22d77
Viktor Bojda <viktorbojda17@gmail.com>
2024-11-20 21:50:18 +0100

    feat: add custom login view and auto-login after registration

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView
-from .views import RegisterView
 from django.contrib.auth.views import LogoutView
-from project.views import HomeView
+
+from project.views import HomeView, LoginView, RegisterView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", HomeView.as_view(), name="home"),
-    path("login/", LoginView.as_view(template_name="auth/login.html"), name="login"),
+    path("login/", LoginView.as_view(), name="login"),
     path("register/", RegisterView.as_view(), name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- project/views.py
+++ project/views.py
@@ -1,11 +1,21 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, login, authenticate
+from django.contrib.auth.views import LoginView as DjangoLoginView
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView, CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django_htmx.http import HttpResponseClientRedirect
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+class LoginView(DjangoLoginView):
+    template_name = "auth/login.html"
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        return HttpResponseClientRedirect(self.get_success_url())
 
 
 class RegisterView(CreateView):
@@ -13,3 +23,12 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = "auth/register.html"
     success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data["username"],
+            password=form.cleaned_data["password1"],
+        )
+        login(self.request, user)
+        return HttpResponseClientRedirect(self.get_success_url())
