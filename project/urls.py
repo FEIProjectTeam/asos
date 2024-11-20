a5c43257305d567bb3e1c033c34c5f5dac355a3d
Peter Bartko <peterbartko132@gmail.com>
2024-11-20 17:46:42 +0100

    login register and logout

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from .views import register
+from django.contrib.auth.views import LogoutView
 from project.views import HomeView
 
 urlpatterns = [
     # path('', views.home, name='home'),  # Maps the root URL to the home view
     path('admin/', admin.site.urls),
     path("", HomeView.as_view(), name="home"),
+    path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('register/', register, name='register'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),  # Redirects to the home page after logout
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created. You can now log in.")
+            return redirect("login")  # Redirect to login after successful registration
+    else:
+        form = UserCreationForm()
+    return render(request, "auth/register.html", {"form": form})
