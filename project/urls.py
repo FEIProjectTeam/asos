c4fd079e42ca7828c2eb0210a530227892a6978e
Peter Bartko <peterbartko132@gmail.com>
2024-11-20 15:31:37 +0100

    base and home html

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from project.views import HomeView
 
 urlpatterns = [
+    # path('', views.home, name='home'),  # Maps the root URL to the home view
     path('admin/', admin.site.urls),
+    path("", HomeView.as_view(), name="home"),
 ]
